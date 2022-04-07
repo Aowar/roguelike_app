a3d3fefbe3ed6da101e48ac0dd8df07bd868888d
Alex <lesha-noskov@mail.ru>
2022-04-07 14:10:10 +0300

    ono?

--- lib/chest.dart
+++ lib/chest.dart
@@ -1,23 +1,30 @@
 import 'dart:math';
 
+import 'weapon.dart';
+import 'armor.dart';
 import 'bobject.dart';
 import 'hero.dart';
 
 class Chest extends Bobject {
-  var item;
+  late var item;
 
-  Chest(x, y) : super(x, y) {
-    Random rng = new Random();
-    int gen = rng.nextInt(2);
-    if(gen == 1) {
-      ///item = new Weapon(число * лвл);
-    }
-    else {
-      ///item = new Armour(число * лвл);
-    }
-  }
+  Chest(x, y) : super(x, y);
 
   void OpenChest(Hero hero) {
+    int gen = Random().nextInt(2);
+    String type = "sword";
+    if(gen == 1) {
+      var wtype = Random().nextInt(2);
+      if (wtype == 1) {
+        type = "spear";
+        item = Weapon(hero.GetLvl() + Random().nextInt(5) + 1, type);
+      } else {
+        item = Weapon(hero.GetLvl() + Random().nextInt(10) + 1, type);
+      }
+    }
+    else {
+      item = Armour(hero.GetLvl() + Random().nextInt(10) + 1);
+    }
     hero.AddToInv(item);
   }
 }
--- lib/weapon.dart
+++ lib/weapon.dart
@@ -1,13 +1,18 @@
 class Weapon {
   int atk;
+  String type;
   //int level;
 
-  Weapon(this.atk) {
+  Weapon(this.atk, this.type) {
     //Random rng = new Random();
     //this.atk = rng.nextInt(10 * level) + level;
   }
 
   int GetWeaponAtk() {
     return atk;
+  }
+
+  String GetWeaponType() {
+    return type;
   }
 }
